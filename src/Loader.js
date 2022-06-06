3242d06063c2a7990a63a89cbe15ae5323dbf2ef
bioss7 <fireleny@gmail.com>
2022-06-06 21:52:32 +0700

    some changes

--- src/Loader.js
+++ src/Loader.js
@@ -1,5 +1,7 @@
 import React from "react";
 
-export default function Loader() {
-    <div style={{display: 'flex', justifyContent: 'center', margin: '.5rem'}}><div className="lds-dual-ring" /></div>
-}
+export default () => {
+  <div style={{ display: "flex", justifyContent: "center", margin: ".5rem" }}>
+    <div className="lds-dual-ring" />
+  </div>;
+};
